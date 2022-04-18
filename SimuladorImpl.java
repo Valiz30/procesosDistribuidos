dab4b4af5f51ffbeaf0bdf55cd4142ab391a7185
Valiz30 <83610345+Valiz30@users.noreply.github.com>
2022-04-17 22:30:36 -0500

    Servidor

--- SimuladorImpl.java
+++ SimuladorImpl.java
@@ -7,17 +7,19 @@ import java.util.logging.Logger;
 
 public class SimuladorImpl extends UnicastRemoteObject
    implements SimuladorInterfaz {
-      final int TOTAL_CLIENTES = 5, TOTAL_PAQUETES = 30; //variables constantes
+      final int TOTAL_CLIENTES = 5, TOTAL_PAQUETES = 30, TOTAL_PROCESOS = 30; //variables constantes
       int contClientAct = 0, contPaquetesAct = 0; //contadores
-      int[] clientes = new int[TOTAL_CLIENTES]; //registro de clientes en ejecucion
+      int[] clientes = new int[TOTAL_CLIENTES], procesosRegistrados; //registro de clientes en ejecucion, 
       Paquete[] paquetesPendientes = new Paquete[TOTAL_PAQUETES];// registro de paquetes/procesos que esperan a ser ejecutados
+      Procesos[] procesosExistentes = new Procesos[TOTAL_PROCESOS];
+
    public SimuladorImpl() throws RemoteException{
       super();
 
    }
-   public int registrar(String name, ){//registra CLIENTE, cuando es creado.
+   public int registrar(String nombre){//registra CLIENTE, cuando es creado.
       int indice = 0;
-      clientes[indice] = contClientAct; //se añade el indice del cliente al registro
+     // clientes[indice] = contClientAct; //se añade el indice del cliente al registro
       contClientAct++;
       return clientes[indice];// regreso el identificador del cliente
    }
@@ -27,14 +29,19 @@ public class SimuladorImpl extends UnicastRemoteObject
    }
    public boolean[] actualizarProceso(int[] idProcesos){//verificar si los proceso pendientes del cliente han terminado de ejecutarse
       boolean[] verProceso = new boolean[idProcesos.length];//arreglo que indica si el proceso ya se ejecuto, respetando los indices en el arreglo idProcesos
+
       return verProceso;
    }
    public void recibir(Paquete paquete){//si un proceso llega a un cliente, le avisa al servidor mediante esta funcion.
       if(contPaquetesAct < 30){ //la cantidad de procesos maxima a recibir de forma general, es de 30.
-         paquetesPendientes[contPaquetesAct] = paquete; //guarda el proceso/paquete que el cliente envia
+         paquetesPendientes[contPaquetesAct] = paquete; //guarda el proceso/paquete que el cliente envia como proceso listo a ejecutar
+         procesosExistentes[contPaquetesAct] = paquete.proceso;
       }else{
          
       }
+   }
+   public void enviarProcesoTerminado(Procesos procesoTerminado){
+
    }
    public void administrarProceso(){//se encarga de administrar el destino de los procesos a ejecutar
 
